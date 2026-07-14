2fb0efa27a38c822469690cefa69579aeb02a221
Nobody <127373451+Nobodyworld@users.noreply.github.com>
2026-07-14 13:06:15 -0500

    feat: export Census adapter error

--- src/adapters/__init__.py
+++ src/adapters/__init__.py
@@ -2,11 +2,12 @@
 
 from .aies import AIESDataError, fetch_latest_aies_snapshot
 from .bea import BEAClientError, fetch_go_ii_by_industry, select_bea_endpoint
-from .census_asm import fetch_asm_manufacturing
+from .census_asm import CensusASMClientError, fetch_asm_manufacturing
 
 __all__ = [
     "AIESDataError",
     "BEAClientError",
+    "CensusASMClientError",
     "fetch_latest_aies_snapshot",
     "fetch_go_ii_by_industry",
     "fetch_asm_manufacturing",
